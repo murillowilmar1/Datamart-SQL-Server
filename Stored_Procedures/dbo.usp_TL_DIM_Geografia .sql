e15d8f855878411e94700bacd0fb1ce43f51f296
murillowilmar1 <murillowilmar1@homtail.com>
2023-04-28 18:42:10 -0500

    procGeo2

--- Stored_Procedures/dbo.usp_TL_DIM_Geografia .sql
+++ Stored_Procedures/dbo.usp_TL_DIM_Geografia .sql
@@ -3,23 +3,54 @@ AS
 
 ---DUMMY 
 
+IF NOT EXISTS (SELECT IdGeografia FROM [Datamart_Norte].[dbo].[DIM_Geografia]
+					WHERE IdGeografia = -99)
+BEGIN
+	--Apague el Identity
+	SET IDENTITY_INSERT [Datamart_Norte].[dbo].[DIM_Geografia] ON 
+	             
 
 
+INSERT INTO [Datamart_Norte].[dbo].[DIM_Geografia]
+           ([IdGeografia]
+		   ,[CodGeografia]
+           ,[CodCiudad]
+           ,[Ciudad_Nombre]
+           ,[CodRegion]
+           ,[Region_Nombre]
+           ,[CodPais]
+           ,[Pais_Nombre]
+           ,[BI_Control_Extraccion ]
+           ,[BI_Control_Transformacion])
+     VALUES
+           (
+            -99
+           ,-99
+           ,-99
+           ,'_Sin Ciudad'
+           ,-99
+           ,'_Sin Region'	
+           ,-99
+           ,'_Sin Pais' 
+		   ,GETDATE()
+		   ,GETDATE())
+	--Encienda el Identity
+     SET IDENTITY_INSERT [Datamart_Norte].[dbo].[DIM_Geografia] OFF 
 
-
---- UPDATE 
-
-
-
-
-
-
-
-
-
+END
 
 ---- INSERT TO 
 
+INSERT INTO [Datamart_Norte].[dbo].[DIM_Geografia]
+           ([CodGeografia]
+           ,[CodCiudad]
+           ,[Ciudad_Nombre]
+           ,[CodRegion]
+           ,[Region_Nombre]
+           ,[CodPais]
+           ,[Pais_Nombre]
+           ,[BI_Control_Extraccion ]
+           ,[BI_Control_Transformacion])
 
 
 (SELECT    Stg.[CodGeografia]
@@ -31,12 +62,13 @@ AS
 		  ,Stg.[Pais_Nombre]
 		  ,Stg.[BI_Control_Extraccion ]
 		  ,Stg.[BI_Control_Transformacion]
-         INTO [Datamart_Norte].[dbo].[DIM_Geografia]
+        -- INTO [Datamart_Norte].[dbo].[DIM_Geografia]
   FROM[Staging_Norte].[dbo].[VW_Tr_Geografia] AS Stg
-  
-  
-  ) 
+      LEFT JOIN [Datamart_Norte].[dbo].[DIM_Geografia] AS Dtm 
+	  ON Stg.CodGeografia = Dtm.CodGeografia 
+	  WHERE Dtm.CodGeografia IS NULL ) 
 
 GO
+
 
 
